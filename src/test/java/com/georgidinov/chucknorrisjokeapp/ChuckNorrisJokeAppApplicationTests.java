3af9009d981807d50fec304527c4c84b57c16225
Georgi Dinov <georgidinov1@abv.bg>
2020-07-22 11:08:38 +0300

    Initial Commit

---
+++ src/test/java/com/georgidinov/chucknorrisjokeapp/ChuckNorrisJokeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.georgidinov.chucknorrisjokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckNorrisJokeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
